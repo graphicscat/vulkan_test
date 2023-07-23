0a04a10978ec191b66a7f4f0ca6c4c153034387e
lyj <lyj>
2023-07-23 15:02:08 +0800

    change

---
+++ src/vk_engine.cpp
@@ -0,0 +1,21 @@
+#include <vk_engine.h>
+
+void VulkanEngine::init(){}
+
+void VulkanEngine::cleanup(){}
+
+void VulkanEngine::draw(){}
+
+void VulkanEngine::run(){}
+
+void VulkanEngine::init_vulkan(){}
+
+void VulkanEngine::init_swapchain(){}
+
+void VulkanEngine::init_default_renderpass(){}
+
+void VulkanEngine::init_framebuffers(){}
+
+void VulkanEngine::init_commands(){}
+
+void VulkanEngine::init_sync_structures(){}
